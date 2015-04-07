c63637e4e09ef08cf32429a100217e7676096096
j0narch3r <jonarcher@pobox.com>
2015-04-07 13:07:56 -0700

    adds Age of Laika solution

--- AgeOfLaika/ViewController.swift
+++ AgeOfLaika/ViewController.swift
@@ -10,6 +10,11 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var humanYearsTextField: UITextField!
+    @IBOutlet weak var dogYearsLabel: UILabel!
+    
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
@@ -20,6 +25,13 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
+    @IBAction func convertToDogYearsButtonPressed(sender: UIButton) {
+        
+        let humanYears = humanYearsTextField.text.toInt()!
+        dogYearsLabel.text = "Laika is " + "\(humanYears)" + " in Dog Years"
+        humanYearsTextField.text = ""
+        humanYearsTextField.resignFirstResponder()
+    }
 
 }
 
