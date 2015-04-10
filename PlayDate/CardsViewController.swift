5c93bf359f46da01926e6ee0c2d52b5f9185e110
j0narch3r <jonarcher@pobox.com>
2015-04-10 15:41:44 -0700

    adds code through to just before parse init

---
+++ PlayDate/CardsViewController.swift
@@ -0,0 +1,68 @@
+//
+//  CardsViewController.swift
+//  PlayDate
+//
+//  Created by Jonathan Archer on 4/10/15.
+//  Copyright (c) 2015 Jonathan Archer. All rights reserved.
+//
+
+import UIKit
+
+class CardsViewController: UIViewController, SwipeViewDelegate {
+    
+    struct Card {
+        let cardView: CardView
+        let swipeView: SwipeView
+    }
+    
+    let frontCardTopMargin: CGFloat = 0
+    let backCardTopMargin: CGFloat = 10
+    
+    @IBOutlet weak var cardStackView: UIView!
+    
+    var backCard: Card?
+    var frontCard: Card?
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+        
+        cardStackView.backgroundColor = UIColor.clearColor()
+        
+        backCard = createCard(backCardTopMargin)
+        cardStackView.addSubview(backCard!.swipeView)
+        
+        frontCard = createCard(frontCardTopMargin)
+        cardStackView.addSubview(frontCard!.swipeView)
+        
+    }
+    
+    private func createCardFrame(topMargin: CGFloat)->CGRect{
+        return CGRect(x: 0, y: topMargin, width: cardStackView.frame.width, height: cardStackView.frame.height)
+    }
+    
+    private func createCard(topMargin: CGFloat) -> Card {
+        let cardView = CardView()
+        let swipeView = SwipeView(frame: createCardFrame(topMargin))
+        swipeView.delegate = self
+        swipeView.innerView = cardView
+        return Card(cardView: cardView, swipeView: swipeView)
+    }
+    
+    // MARK: SwipeViewDelegate
+    
+    func swipedLeft() {
+        println("left")
+        if let frontCard = frontCard {
+            frontCard.swipeView.removeFromSuperview()
+        }
+    }
+    
+    func swipedRight() {
+        println("right")
+        if let frontCard = frontCard {
+            frontCard.swipeView.removeFromSuperview()
+        }
+    }
+
+}
