9d0e2f445405989acead604ddda48435aca84a78
j0narch3r <jonarcher@pobox.com>
2015-04-14 11:37:12 -0700

    updates profileViewController, downloads photo and stuff

--- PlayDate/LoginViewController.swift
+++ PlayDate/LoginViewController.swift
@@ -33,20 +33,32 @@ class LoginViewController: UIViewController {
             else if user.isNew {
                 println("User signed up and logged in through Facebook!")
                 
+                //Initialize FB params
+                
                 FBRequestConnection.startWithGraphPath("/me?fields=picture,first_name,birthday,gender", completionHandler: {
                     connection, result, error in
                     var r = result as NSDictionary
                     user["firstName"] = r["first_name"]
                     user["gender"] = r["gender"]
-                    user["picture"] = ((r["picture"] as NSDictionary)["data"] as NSDictionary) ["url"]
+                    
                     var dateFormatter = NSDateFormatter()
                     dateFormatter.dateFormat = "MM/dd/yyyy"
                     user["birthday"] = dateFormatter.dateFromString(r["birthday"] as String)
-                    user.saveInBackgroundWithBlock({
-                        success, error in
-                        println(success)
-                        println(error)
+                   
+                    
+                    let pictureURL = ((r["picture"] as NSDictionary)["data"] as NSDictionary) ["url"] as String
+                    let url = NSURL(string: pictureURL)
+                    let request = NSURLRequest(URL: url!)
+                    
+                    NSURLConnection.sendAsynchronousRequest(request, queue: NSOperationQueue.mainQueue(), completionHandler: {
+                        response, data, error in
+                        
+                        let imageFile = PFFile(name: "avatar.jpg", data: data)
+                        user["picture"] = imageFile
+                        user.saveInBackgroundWithBlock(nil)
                     })
+                    
+                    
                     }
                 )
                 
---
+++ PlayDate/ProfileViewController.swift
@@ -0,0 +1,38 @@
+//
+//  ProfileViewController.swift
+//  PlayDate
+//
+//  Created by Jonathan Archer on 4/14/15.
+//  Copyright (c) 2015 Jonathan Archer. All rights reserved.
+//
+
+import UIKit
+
+class ProfileViewController: UIViewController {
+
+    @IBOutlet weak var imageView: UIImageView!
+    @IBOutlet weak var nameLabel: UILabel!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+        
+        //Updates Profile view with User data
+        nameLabel.text = currentUser()?.name
+        currentUser()?.getPhoto({
+            image in
+            self.imageView.layer.masksToBounds = true
+            self.imageView.contentMode = .ScaleAspectFill
+            self.imageView.image = image
+            }
+        )
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+}
